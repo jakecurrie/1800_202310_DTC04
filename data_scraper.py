9e33683a375221a3a74c7ac764464cc62c344750
Jacob Currie <jcurrie42@my.bcit.ca>
2023-02-21 22:20:48 -0800

    add additional product type and website

--- data_scraper.py
+++ data_scraper.py
@@ -15,14 +15,25 @@ options.add_argument("--disable-dev-shm-usage")
 
 # Creating a dictionary to store urls for each product type at each store
 urls = {
-    "shovel": {
-    "canadian tire": "https://www.canadiantire.ca/en/cat/outdoor-living/snow-removal-equipment-tools/snow-shovels-rakes-DC0001703.html"}}
+    "Shovels": {
+        "Canadian Tire": "https://www.canadiantire.ca/en/cat/outdoor-living/snow-removal-equipment-tools/snow-shovels-rakes-DC0001703.html",
+        "Lowe's": "https://www.lowes.ca/dept/snow-shovels-snow-winter-essentials-outdoor-a1255?display=100&sort=ordernumber_i%3Adesc"},
+    "Ice Melters": {
+        "Canadian Tire": "https://www.canadiantire.ca/en/cat/outdoor-living/snow-removal-equipment-tools/ice-melters-DC0001701.html",
+        "Lowe's": "https://www.lowes.ca/dept/ice-melt-snow-winter-essentials-outdoor-a827?display=100&sort=ordernumber_i%3Adesc"}
+    }
 
 # Creating a dictionary to store class names used to identify and extract data for each website
 selectors = {
-    "canadian tire": {
-    "parent": "[class='nl-product__content']", 
-    "child_selectors": {"name": "[class='shiitake-children']", "price": "[class='nl-price--total']"}}}
+    "Canadian Tire": {
+        "parent": "[class='nl-product__content']", 
+        "child_selectors": {
+            "name": "[class='shiitake-children']", "price": "[class='nl-price--total']"}},
+    "Lowe's": {
+        "parent": "[class='plp-card-wrapper']",
+        "child_selectors": {
+            "name": "[class='styles__ProductTitle-RC__sc-od4z67-9 kTOEzK card-text']", "price": "[class='price-actual']"}}
+    }
 
 # Initializing the web browser to load web pages
 driver = webdriver.Chrome(options=options)
