15abca95550549fe3e2058dcfa367688095fad23
Joanne Ho <cyh66368@gmail.com>
2023-02-27 17:41:50 -0800

    Merge product page html&css with script

--- scripts/get_products_type.js
+++ scripts/get_products_type.js
@@ -5,43 +5,53 @@ function queryProductCategory() {
   const productList = document.getElementById("product-list");
   const categoryHeader = document.getElementById("category-header");
 
-  
   categoryHeader.innerHTML = category;
 
   const db = firebase.firestore();
-  const productsRef = db.collection("products").where("product_type", "==", category);
-  productsRef.get().then((querySnapshot) => {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    productList.appendChild(row);
+  const productsRef = db
+    .collection("products")
+    .where("product_type", "==", category);
+  productsRef
+    .get()
+    .then((querySnapshot) => {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      productList.appendChild(row);
 
-    let cardCount = 0;
-    querySnapshot.forEach((doc) => {
-      const product = doc.data();
-      const productCard = `
-        <div class="col-sm-4">
-          <div class="card">
-            <img class="card-img-top" src="${product.image_url}" alt="${product.product_name}">
-            <div class="card-body">
-              <h5 class="card-title">${product.product_name}</h5>
-              <p class="card-price">${product.price}</p>
-            </div>
+      let cardCount = 0;
+      querySnapshot.forEach((doc) => {
+        const product = doc.data();
+        const productCard = `
+      <div class="col-md-6 col-lg-4 col-xl-3">
+        <div id="product-${doc.id}" class="single-product">
+          <div class="part-1">
+          <img class="card-img-top" src="${product.image_url}" alt="${product.product_name}">
+            <ul>
+              <li><a href="#"><i class="fa fa-heart"></i></a></li>
+              <li><a href="#"><i class="fa fa-link"></i></a></li>
+            </ul>
+          </div>
+          <div class="part-2">
+            <h3 class="product-price">$${product.price}</h3>
+            <h4 class="product-title">${product.product_name}</h4>
+            <h4 class="product-store">${product.store_name}</h4>
           </div>
         </div>
-      `;
-      row.innerHTML += productCard;
-      cardCount++;
+      </div>
+    `;
+        row.innerHTML += productCard;
+        cardCount++;
 
-      if (cardCount % 3 === 0) {
-        row = document.createElement("div");
-        row.classList.add("row");
-        productList.appendChild(row);
-      }
+        if (cardCount % 3 === 0) {
+          row = document.createElement("div");
+          row.classList.add("row");
+          productList.appendChild(row);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
-  }).catch((error) => {
-    console.error(error);
-  });
 }
 
-
-queryProductCategory()
+queryProductCategory();
