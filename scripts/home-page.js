63fd29062556d88ae787309c90bac6cabbcc7adb
SeungJae <sbj7788@gmail.com>
2023-03-22 21:16:28 -0700

    upload wishlist and update homepage

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -4,21 +4,21 @@ const logo = document.querySelector('.navbar-logo');
 const navbarTitle = document.querySelector('.navbar-title');
 
 window.addEventListener('scroll', () => {
-    yPos = window.scrollY;
-    if (yPos < 100) {
-        navbar.style.backgroundColor = 'transparent';
-        navbarTitle.style.display = 'none';
-        logo.style.opacity = '0.5';
-    } else {
-        navbar.style.backgroundColor = '#263159';
-        navbarTitle.style.display = 'block';
-        logo.style.opacity = '1';
-    }
+  yPos = window.scrollY;
+  if (yPos < 100) {
+    navbar.style.backgroundColor = 'transparent';
+    navbarTitle.style.display = 'none';
+    logo.style.opacity = '0.5';
+  } else {
+    navbar.style.backgroundColor = '#263159';
+    navbarTitle.style.display = 'block';
+    logo.style.opacity = '1';
+  }
 });
 
 // clicking logo and name redirect to home page
-const navbarLogoSection = document.querySelector('.logo-section')
+const navbarLogoSection = document.querySelector('.logo-section');
 
 navbarLogoSection.addEventListener('click', () => {
-    window.location.replace('/test-home-page.html')
+  window.location.replace('/home-page.html');
 });
